7f39736c558c2950be2cc5899b313156e0964566
Hermawan Sentyaki <52445704+hermawans@users.noreply.github.com>
2020-08-30 08:50:31 +0700

    mengganti variabel di Range.py

--- Range.py
+++ Range.py
@@ -1,9 +1,9 @@
-def Range(start=0, end=0, step=1) :
-    if start > end :
-        start, end = end, start
-    while start < end :
-        yield start
-        start += step
+def Range(mulai=0, akhir=0, langkah=1) :
+    if muali > akhir:
+        mulai, akhir = akhir, mulai
+    while mulai < akhir:
+        yield mulai
+        mulai += langkah
 
 for i in Range(10) :
     print(i)
